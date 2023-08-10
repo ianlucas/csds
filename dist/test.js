8e53f4982664b1fb88f856e8e4fd549a83c7a760
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-08-10 19:38:14 -0300

    sync

--- dist/test.js
+++ dist/test.js
@@ -1,9 +1,10 @@
 import { CSGODS, CSGODSState } from "./csds.js";
 const csgods = new CSGODS("linux", "/home/ianlucas");
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 csgods.on("progress", (progress) => {
     console.log(progress);
 });
-csgods.on("state", (state) => {
+csgods.on("state", async (state) => {
     console.log(state);
     if (state === CSGODSState.READY) {
         csgods.start();
@@ -11,6 +12,8 @@ csgods.on("state", (state) => {
     if (state === CSGODSState.ON) {
         console.log(csgods.localIpAddress);
         console.log(csgods.publicIpAddress);
+        await sleep(10000);
+        csgods.console("exit");
     }
 });
 await csgods.initialize();
